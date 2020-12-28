b478085c89db64d2becc0c7ab0c92c0163338089
Tijani <ayomidetijani6@gmail.com>
2020-12-28 20:55:45 +0100

    added a section for the form

--- contests/forms.py
+++ contests/forms.py
@@ -9,7 +9,8 @@ class UploadQrcodeForm(forms.Form):
 class CreateContestForm(forms.Form):
     name = forms.CharField(max_length=100, widget=TextInput(attrs={'class':'form-control', 'placeholder':'Enter title of Contest', 'required':'required'}))
     description = forms.CharField(widget=forms.Textarea(attrs={'class':'form-control', 'placeholder':'Simple Description of contest', 'required':'required'}))
+    maximun_contestant = forms.IntegerField(widget=forms.NumberInput(attrs={'class':'form-control'}))
 
     class meta:
         name = Contest
-        fields = ['name', 'description']
+        fields = ['name', 'description', 'max_contestants']
